fa2fc4b0ecce40e47dcd35bfb675f425f0006a6d
alejandroi <dialejandroi@gmail.com>
2022-08-24 20:45:59 -0300

    commit

--- apps/comentario/admin.py
+++ apps/comentario/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import comentario
+
 # Register your models here.
 admin.site.register(comentario)
--- apps/comentario/urls.py
+++ apps/comentario/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name="comentario"
 
 urlpatterns = [
-    path('ComentariosMain',views.add_comment_to_post, name = 'add_comment_to_post'),
-    
-]
+        path('ComentariosMain',views.add_comment_to_post, name = 'add_comment_to_post'),
+  ]
