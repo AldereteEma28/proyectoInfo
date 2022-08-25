a1d21735f3a781fafab8f42794b51030064a22d2
AldereteEma28 <88401301+AldereteEma28@users.noreply.github.com>
2022-08-24 23:06:20 -0300

    add to Heroku

--- source/settings/prod.py
+++ source/settings/prod.py
@@ -6,10 +6,10 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'project',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'NAME': 'dhndv1itek5si',
+        'USER': 'irpjycantffodk',
+        'PASSWORD': '07be5a56ed656272d020457938f0d65ad6b0b9da57a678b206c9a83ed290f208',
+        'HOST': 'ec2-44-209-186-51.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
