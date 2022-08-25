8147e5c31d2aa9b206d1126f73d98c54031406e3
AldereteEma28 <88401301+AldereteEma28@users.noreply.github.com>
2022-08-25 11:14:58 -0300

    modificacion database

--- source/settings/prod.py
+++ source/settings/prod.py
@@ -8,10 +8,10 @@ CSRF_TRUSTED_ORIGINS = ['https://blog-uocra-grupo6.herokuapp.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dhndv1itek5si',
-        'USER': 'irpjycantffodk',
-        'PASSWORD': '07be5a56ed656272d020457938f0d65ad6b0b9da57a678b206c9a83ed290f208',
-        'HOST': 'ec2-44-209-186-51.compute-1.amazonaws.com',
+        'NAME': 'dt6pjc5j5ij3f',
+        'USER': 'lexswfjynwsdvv',
+        'PASSWORD': '34ff2e5663dfded280091e459d2b1b58aa7cbffa9870a56e88faa0c14f1f63b0',
+        'HOST': 'ec2-52-200-5-135.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
